e35ac051df2754c3253b4e6eeff069d631537793
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2019-06-19 22:13:55 +0300

    change default annotator output

--- qald/annotator/constants.py
+++ qald/annotator/constants.py
@@ -1,5 +1,5 @@
 # INPUT_FILE_PATH = 'datasets/questions/qald8/data/qald8_questions.txt'
 INPUT_FILE_PATH = r'datasets\questions\created\questions list.txt'
-OUTPUT_FILE_PATH = 'annotator/output_new_created.json'
+OUTPUT_FILE_PATH = 'annotator/output2.json'
 
 
