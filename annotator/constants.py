a9d46af6bd0f6e5109413f6bec1480a06471b5be
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-02-09 21:41:08 +0200

    Fix annotator non-existent output file.

--- annotator/constants.py
+++ annotator/constants.py
@@ -1,5 +1,5 @@
 INPUT_FILE_PATH = 'datasets\questions\qald8\data\qald9_questions_with_relations.txt'
 # INPUT_FILE_PATH = r'datasets\questions\created\questions list.txt'
-OUTPUT_FILE_PATH = 'annotator\qald_annotated_trees.json'
+OUTPUT_FILE_PATH = 'annotator\annotator_output.json'
 
 
