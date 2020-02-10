bf8c4e6f0448653eb607677adad9b069f544fbb2
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-02-10 20:53:14 +0200

    add TOPN node handler

--- services/query_generator/node_handlers/argfunc.py
+++ services/query_generator/node_handlers/argfunc.py
@@ -3,38 +3,25 @@ from services.query_generator.constants import ENTITY_SETS
 from services.query_generator.constants import BIND_PATTERN, ARGMAX_PATTERN, ARGMIN_PATTERN, ARGNTH_PATTERN
 
 def handle_ARGMAX(gen, node: QueryTree.Node):
-    handle_TOPN(gen, node, 1)
+    handle_topn_helper(gen, node, 1)
 
-def handle_TOPN(gen, node: QueryTree.Node, limit: int):
-    entity_sets = list(filter(lambda child: child.type.value in ENTITY_SETS, node.children))
-    relation_uri = '<yoyster relation>'
-    if len(entity_sets) == 1: # Subquery
-        gen.node_vs_reference[node] = gen.node_vs_reference[entity_sets[0]]
-        handle_subquery(gen, node, entity_sets[0], asc=False, ordinal=None, limit=limit)
-    elif len(entity_sets) == 0: # Type only
-        gen.node_vs_reference[node] = gen.generate_variable_name()
-        handle_subquery(gen, node, entity_sets[0], asc=False, ordinal=None, limit=limit)
-    elif len(entity_sets) == 2 and entity_sets[0].type == NodeType.ENTITY and entity_sets[1].type == NodeType.ENTITY:
-        e1, e2 = entity_sets
-        handle_2_entities(generator, node, e1, e2, relation_uri, '>')
-    else:
-        # TODO handle union of arbitrary length entity sets
-        print("Unsupported ARGMAX!")
-        raise
-    gen.add_type_restrictions(node)
+def handle_TOPN(gen, node: QueryTree.Node):
+    # literal = list(filter(lambda child: child.type = NodeType.LITERAL, node.children))[0]\
+    literal = 4
+    return handle_topn_helper(gen, node, literal)
 
 def handle_ARGMIN(gen, node: QueryTree.Node):
     entity_sets = list(filter(lambda child: child.type.value in ENTITY_SETS, node.children))
     relation_uri = '<yoyster relation>'
     if len(entity_sets) == 1: # Subquery
         gen.node_vs_reference[node] = gen.node_vs_reference[entity_sets[0]]
-        handle_subquery(gen, node, entity_sets[0], asc=True)
+        handle_subquery(gen, node, asc=True)
     elif len(entity_sets) == 0: # Type only
         gen.node_vs_reference[node] = gen.generate_variable_name()
-        handle_subquery(gen, node, entity_sets[0], asc=True)
+        handle_subquery(gen, node, asc=True)
     elif len(entity_sets) == 2 and entity_sets[0].type == NodeType.ENTITY and entity_sets[1].type == NodeType.ENTITY:
         e1, e2 = entity_sets
-        handle_2_entities(generator, node, e1, e2, relation_uri, '<')
+        handle_2_entities(gen, node, e1, e2, relation_uri, '<')
     else:
         # TODO handle union of arbitrary length entity sets
         print("Unsupported ARGMIN!")
@@ -46,10 +33,11 @@ def handle_ARGNTH(gen, node: QueryTree.Node):
     # literal = list(filter(lambda child: child.type = NodeType.LITERAL, node.children))[0]\
     literal = '<yoyster literal>'
     if len(entity_sets) == 1:
-        handle_subquery(gen, node, entity_sets[0], asc=True, ordinal=literal)
+        gen.node_vs_reference[node] = gen.node_vs_reference[entity_sets[0]]
+        handle_subquery(gen, node, asc=True, ordinal=literal)
     elif len(entity_sets) == 0: # Type only
         gen.node_vs_reference[node] = gen.generate_variable_name()
-        handle_subquery(gen, node, entity_sets[0], asc=True, ordinal=literal)
+        handle_subquery(gen, node, asc=True, ordinal=literal)
     else:
         # TODO handle union of arbitrary length entity sets
         print("Unsupported ARGNTH!")
@@ -60,11 +48,11 @@ def handle_2_entities(gen, node, e1, e2, relation, sign):
     relation_uri = '<yoyster relation>'
     v1, v2 = gen.generate_variable_name(), gen.generate_variable_name()
     gen.node_vs_reference[node] = gen.generate_variable_name()
-    gen.triples.append((e1, relation_uri, e1_val))
-    gen.triples.append((e2, relation_uri, e2_val))
+    gen.triples.append((e1, relation_uri, v1))
+    gen.triples.append((e2, relation_uri, v2))
     gen.filters.append(BIND_PATTERN.format(v1, sign, v2, e1, e2, gen.node_vs_reference[node]))
 
-def handle_subquery(gen, node, subquery_node, asc=True, ordinal=None, limit=1):
+def handle_subquery(gen, node, asc=True, ordinal=None, limit=1):
     relation_uri = '<yoyster relation>'
     val = gen.generate_variable_name() 
     gen.triples.append((gen.node_vs_reference[node], relation_uri, val))
@@ -75,3 +63,21 @@ def handle_subquery(gen, node, subquery_node, asc=True, ordinal=None, limit=1):
     else:
         gen.post_processing.append(ARGMAX_PATTERN.format(val, limit))
 
+
+def handle_topn_helper(gen, node: QueryTree.Node, limit: int):
+    entity_sets = list(filter(lambda child: child.type.value in ENTITY_SETS, node.children))
+    relation_uri = '<yoyster relation>'
+    if len(entity_sets) == 1: # Subquery
+        gen.node_vs_reference[node] = gen.node_vs_reference[entity_sets[0]]
+        handle_subquery(gen, node, asc=False, ordinal=None, limit=limit)
+    elif len(entity_sets) == 0: # Type only
+        gen.node_vs_reference[node] = gen.generate_variable_name()
+        handle_subquery(gen, node, asc=False, ordinal=None, limit=limit)
+    elif len(entity_sets) == 2 and entity_sets[0].type == NodeType.ENTITY and entity_sets[1].type == NodeType.ENTITY:
+        e1, e2 = entity_sets
+        handle_2_entities(gen, node, e1, e2, relation_uri, '>')
+    else:
+        # TODO handle union of arbitrary length entity sets
+        print("Unsupported ARGMAX!")
+        raise
+    gen.add_type_restrictions(node)
