8b710957906dd80684db352aca8a69e84ec11ded
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-02-10 18:52:28 +0200

    Fix constant paths

--- services/query_generator/constants.py
+++ services/query_generator/constants.py
@@ -1,11 +1,13 @@
 QUERY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\query_template.sparql'
-COUNT_ARG_SUBQUERY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\count_subquery_template.sparql'
-COUNT_PROPERTY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\count_property.template.sparql'
+ARGFUNC_SUBQUERY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\argfunc_subquery_template.sparql'
+COUNT_SUBQUERY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\count_subquery_template.sparql'
+COUNT_ENTITY_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\count_entity_template.sparql'
+EXISTS_TEMPLATE_FILE_PATH = r'services\query_generator\static_files\exists_template.sparql'
 ENTITY_SETS = {'ARGMAX', 'ARGMIN', 'ARGNTH', 'ARGMAXCOUNT', 'ARGMINCOUNT', 'SAMPLE', 'GREATER', 'LESS', 'GREATERCOUNT', 'LESSCOUNT', 'TOPN', 'PROPERTY', 'ENTITY'}
 TYPE_RELATION = 'a'
 
 TRIPLE_PATTERN = '\t{} {} {}.\n'
 BIND_PATTERN   = '\tBIND(IF({} {} {}, {}, {}) AS {})\n'
-ARGMAX_PATTERN = 'ORDER BY DESC({}) LIMIT 1.\n'
+ARGMAX_PATTERN = 'ORDER BY DESC({}) LIMIT {}.\n'
 ARGMIN_PATTERN = 'ORDER BY ASC({}) LIMIT 1.\n'
 ARGNTH_PATTERN = 'ORDER BY ASC({}) OFFSET {} LIMIT 1.\n'
--- services/query_generator/query_generator.py
+++ services/query_generator/query_generator.py
@@ -3,10 +3,11 @@ import os
 import sys
 sys.path.insert(0, os.getcwd())
 from common.query_tree import QueryTree, NodeType
-from services.query_generator.constants import QUERY_TEMPLATE_FILE_PATH, TYPE_RELATION, ENTITY_SETS
+from services.query_generator.constants import QUERY_TEMPLATE_FILE_PATH, EXISTS_TEMPLATE_FILE_PATH, TYPE_RELATION, ENTITY_SETS
 from services.query_generator.node_handlers.argfunc import handle_ARGMAX, handle_ARGMIN, handle_ARGNTH
 from services.query_generator.node_handlers.misc import handle_ROOT, handle_PROPERTY, handle_ENTITY
 from services.query_generator.node_handlers.count import handle_ARGMAXCOUNT, handle_ARGMINCOUNT, handle_COUNT
+from services.query_generator.node_handlers.exists import handle_EXISTS, handle_EXISTSRELATION
 
 TRIPLE_PATTERN = '\t{} {} {}.\n'
 NODE_HANDLERS = {
@@ -18,33 +19,38 @@ NODE_HANDLERS = {
     'ENTITY': handle_ENTITY,
     'ARGMAXCOUNT': handle_ARGMAXCOUNT,
     'ARGMINCOUNT': handle_ARGMINCOUNT,
-    # 'COUNT': handle_COUNT,
-    # 'EXISTS': handle_EXISTS,
-    # 'EXISTSRELATION': handle_EXISTSRELATION,
-    # 'SAMPLE': handle_SAMPLE,
-    # 'ISGREATER': handle_ISGREATER,
-    # 'ISLESS': handle_ISLESS,
-    # 'GREATER': handle_GREATER,
-    # 'LESS': handle_LESS,
-    # 'TOPN': handle_TOPN,
-    # 'GREATERCOUNT': handle_GREATERCOUNT,
-    # 'LESSCOUNT': handle_LESSCOUNT,
+    'COUNT': handle_COUNT,
+    'EXISTS': handle_EXISTS,
+    'EXISTSRELATION': handle_EXISTSRELATION,
+    'SAMPLE': handle_SAMPLE
+    'GREATER': handle_GREATER,
+    'LESS': handle_LESS,
+    'TOPN': handle_TOPN,
+    'ISGREATER': handle_ISGREATER,
+    'ISLESS': handle_ISLESS,
+    'GREATERCOUNT': handle_GREATERCOUNT,
+    'LESSCOUNT': handle_LESSCOUNT,
 }
 class QueryGenerator:
 
     def generate(self, query_tree: QueryTree):
+        self.query_tree = query_tree
         self.__handle_node(query_tree.root)
         root_variable = self.node_vs_reference[query_tree.root]
         prefixes = ''
         triples = ''.join([TRIPLE_PATTERN.format(*triple) for triple in self.triples])
         filters = '\n'.join(self.filters)
 
-        with open(QUERY_TEMPLATE_FILE_PATH, 'r', encoding='utf-8') as query_template_file:
+
+        template_file_path = EXISTS_TEMPLATE_FILE_PATH if self.is_exists else QUERY_TEMPLATE_FILE_PATH
+        with open(template_file_path, 'r', encoding='utf-8') as query_template_file:
             template = query_template_file.read()
 
         post_processing = '\n'.join(self.post_processing)
-        
-        final_query = template.format(ROOT_VARIABLE=root_variable, PREFIXES=prefixes, TRIPLES=triples, FILTERS=filters, POST_PROCESS=post_processing)
+        if self.is_exists:
+            final_query = template.format(PREFIXES=prefixes, TRIPLES=triples, FILTERS=filters, POST_PROCESS=post_processing)
+        else:
+            final_query = template.format(ROOT_VARIABLE=root_variable, PREFIXES=prefixes, TRIPLES=triples, FILTERS=filters, POST_PROCESS=post_processing)
 
         return final_query
 
@@ -76,7 +82,9 @@ class QueryGenerator:
         self.__variables = []
         self.triples = []
         self.filters = []
+        self.is_exists = False
         self.post_processing = []
+        self.query_tree = None
 
 QUERY_GENERATOR = QueryGenerator()
 
@@ -90,7 +98,12 @@ INPUT_FILE_PATH = 'datasets\parsing\data\constituency_annotated_questions.json'
 with open(INPUT_FILE_PATH, 'r', encoding='utf-8') as input_file:
     questions = json.load(input_file)
     query_trees = list(map(lambda question: QueryTree.from_dict(question), questions))
-    query_tree = query_trees[838]
+    # query_tree = query_trees[838]
+    # query_tree = query_trees[914]
+    # query_tree = query_trees[247] # Count Property
+    # query_tree = query_trees[1178] # Count Type
+    query_tree = query_trees[20] # Exists
+    query_tree = query_trees[108] # Exists relation Prop-Literal
     query_tree.pretty_print()
     query = QUERY_GENERATOR.generate(query_tree)
     print(query)
