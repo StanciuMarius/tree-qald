d68bbc7f66cf9a13d72d1bdd3e702f0ecd8b5ee4
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-03-05 18:47:08 +0200

    Fixed escaped characters in annotator input file path

--- annotator/constants.py
+++ annotator/constants.py
@@ -1,5 +1,6 @@
 INPUT_FILE_PATH = 'datasets\questions\qald8\data\qald9_questions_with_relations.txt'
+
 # INPUT_FILE_PATH = r'datasets\questions\created\questions list.txt'
-OUTPUT_FILE_PATH = 'annotator\annotator_output.json'
+OUTPUT_FILE_PATH = r'annotator\annotator_output.json'
 
 
