5b5bf599dea983f6ade9aa0fb12533f87cfc7523
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-03-07 15:49:15 +0200

    Added validator (syntax and content) for the equivalent relations dataset.

---
+++ datasets/relation_extraction/cross_kb_relations/validator.py
@@ -0,0 +1,35 @@
+import json
+
+
+valid = True
+duplicates = []
+invalid_keys = []
+with open(r'datasets\relation_extraction\cross_kb_relations\data\equivalent_properties.json', 'r', encoding='utf-8') as file:
+    dataset = json.load(file)
+    dataset['dataset'] = sorted(dataset['dataset'], key=lambda x: x['label'])
+    labels = set()
+
+    relation_vs_label = {}
+    for relation_set in dataset['dataset']:
+        label = relation_set['label']
+        for kb in {'dbpedia', 'freebase', 'tacred', 'wikidata'}:
+            if kb in relation_set:
+                for relation in relation_set[kb]:
+                    if relation in relation_vs_label:
+                        duplicates.append(relation)
+                    else:
+                        relation_vs_label[relation] = label
+        
+        for key in relation_set.keys():
+            if key not in {'label', 'description', 'dbpedia', 'freebase', 'tacred', 'wikidata'}:
+                invalid_keys.append(key)
+if duplicates:
+    print('The following relations appear in multiple entity sets: ', ' '.join(duplicates))
+    valid = False
+
+if invalid_keys:
+    print('Invalid keys found:', ' '.join(invalid_keys))
+    valid = False
+    
+if valid:
+    print('Dataset appears valid!')
