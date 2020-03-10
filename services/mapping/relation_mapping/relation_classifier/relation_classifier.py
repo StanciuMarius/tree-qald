b64f033af38a2673d59474006609f603b41416f8
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-03-10 18:34:32 +0200

    rename file

--- services/mapping/relation_mapping/relation_classifier/relation_classifier.py
+++ services/mapping/relation_mapping/relation_classifier/relation_classifier.py
@@ -14,7 +14,7 @@ class RelationClassifier(object):
         self.inferrer = load_inferrer_from_trained(['--temp_folder_path', constants.TEMP_FOLDER_FOR_SUBMODULE_PATH, '--num_classes', num_classes])
         self.preprocessor = preprocessing.BertRelationExtractionFormatTransform()
 
-    def __call__(self, text, subject_begin, subject_end, object_begin, object_end):
+    def __call__(self, text, subject_begin, subject_end, object_begin, object_end, **kwargs):
         sample = {
             'subject_begin': subject_begin,
             'subject_end': subject_end,
@@ -26,13 +26,3 @@ class RelationClassifier(object):
         probabilities = self.inferrer.infer_one_sentence(sequence, return_probabilities=True)
         return probabilities
         
-# model = RelationClassifier()
-# text         = "Who wrote Michael Jackson's first song?"
-# object_text = "Who"
-# subject_text  = "Michael Jackson's first song"
-# subject_index = text.find(subject_text)
-# object_index = text.find(object_text)
-
-# prediction = model(text, subject_index, subject_index + len(subject_text), object_index, object_index + len(object_text))
-# for x in prediction:
-#     print(x)
