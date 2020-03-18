9015643b01a793019a28ba90ae7d514688dcceb2
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-03-19 00:34:08 +0200

    add rest of the services to app startup list.

--- app.py
+++ app.py
@@ -5,19 +5,23 @@ import os
 def run_services():
     services = []
     
-    services.append(subprocess.Popen(['python', './services/parser/run_parser_service.py']))
-    services.append(subprocess.Popen(['python', './services/nlp/run_nlp_serivce.py']))
+    services.append(subprocess.Popen(['python', r'services\parser\run_parser_service.py']))
+    services.append(subprocess.Popen(['python', r'services\nlp\run_nlp_serivce.py']))
+    services.append(subprocess.Popen(['python', r'services\mapping\run_mapping_service.py']))
+    services.append(subprocess.Popen(['python', r'services\query_generator\run_query_generation_service.py']))
+    services.append(subprocess.Popen(['python', r'services\knowledge_base\run_knowledge_base_service.py']))
     
     return services
 
 def main():
     _ = run_services()
 
-    os.chdir('app')
-    subprocess.call(['npm', 'start'], shell=True)
+    # os.chdir('app')
+    # subprocess.call(['npm', 'start'], shell=True)
     
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, 1000, specification_dir='swagger/')
-    app.add_api('qald.yaml', resolver=RestyResolver('api'))
-    app.run()
+    main()
+    # app = connexion.App(__name__, 1000, specification_dir='swagger/')
+    # app.add_api('qald.yaml', resolver=RestyResolver('api'))
+    # app.run()
