34884e51eb592a9d185fc710aa59f4373568064c
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-04-02 16:37:11 +0300

    Replace legacy blackbox entity mapping system.

--- services/mapping/entity_mapping/entity_mapping.py
+++ services/mapping/entity_mapping/entity_mapping.py
@@ -20,26 +20,60 @@ from nltk.tokenize import word_tokenize
 '''
 class EntityMapping:
     def __init__(self):
-        
-        self.text_vs_resources = {}
+        self.label_vs_entity = {}
 
-        with open(constants.ENTITY_LEXICON_PATH, 'r', encoding='utf-8') as r:
-            lines = r.readlines()
+        if not os.path.exists(constants.ENTITY_LEXICON_PATH):
+            print("No entity lexicon was found. Generating it at {}".format(constants.ENTITY_LEXICON_PATH))
+            self.__generate_lexicon()
+        else:
+            with open(constants.ENTITY_LEXICON_PATH, encoding="utf-8") as file:
+                lines = file.readlines()
+            
             for line in tqdm(lines, desc='Loading entity lexicon'):
-                tokens = line.rstrip('\t\n').split('\t')
+                label, entities = line.split('\t')
+                self.label_vs_entity[label] = entities.split(' ')
+            print("Finished loading entity lexicon!")
 
-                firstCandidate = tokens[1].split(' ')
-                bestCandidate = firstCandidate[0]
-                importance = int(firstCandidate[1])
-                for tok in tokens[2:]:
-                    candidate = tok.lstrip(' ').split(' ')
-                    if int(candidate[1]) > importance:
-                        importance = int(candidate[1])
-                        bestCandidate = candidate[0]
+        # with open(constants.ENTITY_LEXICON_PATH, 'r', encoding='utf-8') as r:
+        #     lines = r.readlines()
+        #     for line in tqdm(lines, desc='Loading entity lexicon'):
+        #         tokens = line.rstrip('\t\n').split('\t')
+
+        #         firstCandidate = tokens[1].split(' ')
+        #         bestCandidate = firstCandidate[0]
+        #         importance = int(firstCandidate[1])
+        #         for tok in tokens[2:]:
+        #             candidate = tok.lstrip(' ').split(' ')
+        #             if int(candidate[1]) > importance:
+        #                 importance = int(candidate[1])
+        #                 bestCandidate = candidate[0]
                 
-                self.text_vs_resources[tokens[0].lower()] = bestCandidate
+        #         self.text_vs_resources[tokens[0].lower()] = bestCandidate
            
-        print("Finished loading entity lexicon!")
+
+    def __generate_lexicon(self):
+        def process_ttl(path):
+            with open(path, 'r', encoding='utf-8') as file:
+                for line in tqdm(file, desc='Processing {}'.format(path)):
+                    try:
+                        label, _, entity, _ = line.split(' ')
+                        label = label.split('/')[-1].replace('>', '').replace('(disambiguation)', '').replace('_', ' ').strip().lower()
+                        entity = entity[29:-1] # get rid of '<http://dbpedia.org/resource/' and '>'
+                        if label in self.label_vs_entity:
+                            self.label_vs_entity[label].append(entity)
+                        else:
+                            self.label_vs_entity[label] = [entity]
+                    except:
+                        print("Error parsing line: " + line)
+                        continue
+
+        process_ttl(constants.ENTITY_REDIRECTS_DATASET_PATH)
+        process_ttl(constants.ENTITY_DISAMBIGUATIONS_DATASET_PATH)
+
+        with open(constants.ENTITY_LEXICON_PATH, 'w', encoding='utf-8') as file:
+            for label, entities in self.label_vs_entity.items():
+                file.write(label + '\t' + ' '.join(entities) + '\n')
+            
 
     def __call__(self, entity_text: str):
         # Preprocess the entity text in various ways, resulting in more versions to try to find in the lexicon
@@ -49,31 +83,32 @@ class EntityMapping:
         # No stopwords
         tokens = word_tokenize(entity_text)
         no_stopwords_tokens = [word for word in tokens if not word in stopwords.words()]
-        no_stopwords = ''.join(no_stopwords_tokens)
+        no_stopwords = ' '.join(no_stopwords_tokens)
         versions.append(no_stopwords)
 
         # Inversions
-        no_stopwords_reversed = ''.join(reversed(no_stopwords_tokens))
+        no_stopwords_reversed = ' '.join(reversed(no_stopwords_tokens))
         versions.append(no_stopwords_reversed)
 
         # Extra word
         for index in range(len(tokens)):
-            removed_word = ''.join(tokens[:index] + tokens[index + 1:])
+            removed_word = ' '.join(tokens[:index] + tokens[index + 1:])
             versions.append(removed_word)
 
 
         result = []
         print("Trying to map: " + '|'.join(versions))
         for version in versions:
-            if version in self.text_vs_resources:
-                result.append(self.text_vs_resources[version])
+            if version in self.label_vs_entity:
+                result = self.label_vs_entity[version]
                 break
         
-        result = [constants.DBPEDIA_RESOURCE_PREFIX + entity for entity in result]
-        print("Result: " + ' '.join(result))
+        result = [constants.DBPEDIA_RESOURCE_PREFIX + entity for entity in result][:1]
+        print("Result: " + '|'.join(result))
     
         return result
 
-# entity_mapper = EntityMapping(ENTITY_LEXICON_PATH)
-# print(entity_mapper.map_entity('hypnotiq'))
-# print(entity_mapper.map_entity('hysterical'))
+    def __test(self):
+        assert self('Hotel California') == ['http://dbpedia.org/resource/Hotel_California']
+        assert self('Ceres') == ['http://dbpedia.org/resource/Ceres']
+        assert self('Obama') == ['http://dbpedia.org/resource/Barack_Obama']
