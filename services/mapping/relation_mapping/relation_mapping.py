a14001aed83d7733b3f6084ae13ecb14eb46c000
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-04-03 21:51:20 +0300

    Consider reverse sequence for relation classification.

--- services/mapping/relation_mapping/relation_mapping.py
+++ services/mapping/relation_mapping/relation_mapping.py
@@ -56,8 +56,10 @@ class RelationMapper(object):
                 classifier_score = classifier_scores[reverse_relation]
             else: 
                 classifier_score = 0.0
-        
-            return self.classifier_weight * classifier_score + self.patty_weight * patty_score / (self.classifier_weight + self.patty_weight)
+
+            # Harmonic mean of the two
+            final_score = self.classifier_weight * classifier_score + self.patty_weight * patty_score / (self.classifier_weight + self.patty_weight)
+            return final_score, classifier_score, patty_score # Last two are for debugging purposes
 
         if not candidates:
             # If no prior candidates are provided, we consider all kb relations
@@ -65,31 +67,36 @@ class RelationMapper(object):
 
         # We pick the highest ranked candidates (multiple candidates if they all have the same score)
         candidates = [(candidate, compute_score(candidate)) for candidate in candidates]
-        candidates = sorted(candidates, key=lambda x: x[1], reverse=True)
-        result = [candidate[0] for candidate in candidates if candidate[1] == candidates[0][1]]
-        print('Picked {}\n from {}'.format('\n'.join(result), '\n'.join(str(candidate) for candidate in candidates)))
+        candidates = sorted(candidates, key=lambda x: x[1][0], reverse=True)
+        result = [candidate[0] for candidate in candidates if candidate[1][0] == candidates[0][1][0]]
+        print('='*20)
+        print("text: " + text)
+        print("subject: " + text[subject_begin:subject_end])
+        print("object: " + text[object_begin:object_end])
+        print('Picked {}\n from \n{}\n'.format('\n'.join(result), '\n'.join(str(candidate) for candidate in candidates[:min(len(candidates), 20)])))
+        print('='*20)
 
         return result
 
     def __test1__(self):
-        text = "Who is the oldest actor that stars in a movie directed by Quentin Tarantino?"
-        subject = "movie"
-        object = "Quentin Tarantino"
+        text = "Give me all actors starring in movies directed by William Shatner."
+        subject = "William Shatner"
+        object = "movies"
         subject_begin = text.find(subject)
         subject_end = subject_begin + len(subject)
 
         object_begin = text.find(object)
         object_end = object_begin + len(object)
         candidates = [
-        "http://dbpedia.org/ontology/director",
-        "http://dbpedia.org/ontology/editor",
-        "http://dbpedia.org/ontology/writer",
-        "http://dbpedia.org/ontology/producer",
-        "http://dbpedia.org/ontology/narrator",
-        "http://dbpedia.org/ontology/basedOn",
-        "http://dbpedia.org/ontology/story",
-        "http://dbpedia.org/ontology/cinematography",
-        "http://dbpedia.org/ontology/starring"
+            "http://dbpedia.org/ontology/director",
+            "http://dbpedia.org/ontology/editor",
+            "http://dbpedia.org/ontology/writer",
+            "http://dbpedia.org/ontology/producer",
+            "http://dbpedia.org/ontology/narrator",
+            "http://dbpedia.org/ontology/basedOn",
+            "http://dbpedia.org/ontology/story",
+            "http://dbpedia.org/ontology/cinematography",
+            "http://dbpedia.org/ontology/starring",
         ]
         kb_relations = self(text, subject_begin, subject_end, object_begin, object_end, candidates)
         assert kb_relations[0] == "http://dbpedia.org/ontology/director"
@@ -110,4 +117,4 @@ class RelationMapper(object):
 if __name__ == '__main__':
     ranker = RelationMapper()
     ranker.__test1__()
-    ranker.__test2__()
+    # ranker.__test2__()
