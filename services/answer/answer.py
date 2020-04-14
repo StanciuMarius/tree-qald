d0bce9a3f9a20cb42477df1a5ee39626bd08b916
Marius-Valeriu STANCIU <marius.stanciu1710@cti.pub.ro>
2020-04-14 22:11:06 +0300

    Add questions to annotate.

--- services/answer/answer.py
+++ services/answer/answer.py
@@ -1,38 +1,30 @@
 import os
 import sys
 from typing import List
+import json
 
 sys.path.insert(0, os.getcwd())
 from common.query_tree import QueryTree, NodeType
 from services.tasks import Task, run_task
-# from services.answer.utils import map_relations
 
 TREES_TO_CONSIDER = 3
 def answer(question_text: str) -> List[str]:
     # Parse the query to obtain a query tree.
     trees = run_task(Task.PARSE, question_text)
+    first_mapped_tree = None
     
     for tree in trees[:TREES_TO_CONSIDER]:
-        query_tree = QueryTree.from_dict(tree)
-        query_tree.pretty_print()
-
+        QueryTree.from_dict(tree).pretty_print()
         query = run_task(Task.GENERATE_QUERY, tree)
-        if not query: continue
+        if not first_mapped_tree: first_mapped_tree = query['tree']
+        QueryTree.from_dict(query['tree']).pretty_print()
+        
+        if not query or not query['query_body']: continue
 
         answer = run_task(Task.RUN_SPARQL_QUERY, query)
-        if answer: return query['query_body'], answer
+        if answer or type(answer) == bool:
+            return answer, query['query_body'], query['tree']
+        else:
+            print("No answer, trying next tree...")
     
-    raise Exception("Could not generate answer")
-
-# query_body, result = answer("Who is the oldest actor that plays in a movie directed by Quentin Tarantino?") # good
-# query_body, result = answer("Who is the youngest actor that plays in a movie directed by Steven Spielberg?")
-# query_body, result = answer("What is the largest country the Danube flows through?") #good
-# query_body, result = answer("What is the longest river that flows through Romania?") #good
-# query_body, result = answer("Give me the 5 largest mountains in Romania.") #good
-# query_body, result = answer("Give me the fifth highest mountain in Romania.") #good
-# query_body, result = answer("How old are Barack Obama's children?") #bad
-# query_body, result = answer("Who is the youngest child fo Barack Obama?") #bad
-query_body, result = answer("Who developed the iPhone?") #bad
-
-print(query_body)
-print(result)
+    return None, None, first_mapped_tree
